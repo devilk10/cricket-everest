f5875e719b35cc495ad7112da82e19fe496d9d56
Ketan Sangle <ksangle24@gmail.com>
2024-03-13 21:38:52 +0530

    Add cards in the data package

--- src/com/ketansa/cricket/Main.kt
+++ src/com/ketansa/cricket/Main.kt
@@ -1,10 +1,27 @@
 package com.ketansa.cricket
 
+import com.ketansa.cricket.data.Cards.battingCards
+import com.ketansa.cricket.data.Cards.bowlingCards
+import com.ketansa.cricket.data.Cards.shotTimings
 import java.util.Scanner
 
 fun main() {
-    val scanner = Scanner(System.`in`)
-    println("Enter some input:")
-    val userInput = scanner.nextLine()
-    println("You entered: $userInput")
+    println("Input action in following format _BOWLING_CARD _BATTING_CARD _SHOT_TIMING")
+    Scanner(System.`in`)
+    val input = readlnOrNull()?.split(" ") ?: return
+    if (input.size != 3) {
+        println("Invalid input format. Please enter all three values separated by spaces.")
+        return
+    }
+
+    val bowlingCard = input[0]
+    val battingCard = input[1]
+    val shotTiming = input[2]
+
+    if (bowlingCard !in bowlingCards || battingCard !in battingCards || shotTiming !in shotTimings) {
+        println("Invalid input. Please enter valid bowling card, shot card, and shot timing.")
+        return
+    }
+
+    println("Predicted Shot Outcome: SOME RANDOM OUTPUT")
 }
