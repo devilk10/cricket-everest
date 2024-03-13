c55ea677e20a42367632afde8d7b14d3edc68ff8
Ketan Sangle <ksangle24@gmail.com>
2024-03-13 21:22:54 +0530

    Initial commit

---
+++ src/com/ketansa/cricket/Main.kt
@@ -0,0 +1,10 @@
+package com.ketansa.cricket
+
+import java.util.Scanner
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    println("Enter some input:")
+    val userInput = scanner.nextLine()
+    println("You entered: $userInput")
+}
